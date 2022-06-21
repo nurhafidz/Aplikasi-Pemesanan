ab8a63674eb5de4180015b95f58a765eeeb047f7
RaihanSyahSP <raihansyah45@gmail.com>
2022-06-21 08:44:55 +0700

    fix db

--- routes/index.route.js
+++ routes/index.route.js
@@ -6,28 +6,28 @@ const PesananController = require("../controllers/pesanan.controller");
 const router = require("express").Router();
 
 router.get("/", indexController.index);
-
-
-// route pemesanan
-router.get("/pesanan", auth, PesananController.index);
-router.get("/pesanan/create", auth, PesananController.create);
-router.post("/pesanan/store", auth, (req, res) => {
-    PesananController.store
-});
-router.get("/pesanan/edit/:id", auth, PesananController.edit);
-router.post("/pesanan/update/:id", auth, (req,res) => {
-    PesananController.update
-});
-router.post("/pesanan/delete/:id", auth, (req, res) => {
-   PesananController.destroy 
-}) 
-
-
 // route login
 const verifyUser = require("../middlewares/verify");
 
 
 // router.get("/", indexController.index);
 router.get("/", verifyUser.isLogin, indexController.index);
+
+
+// route pemesanan
+router.get("/pesanan", verifyUser.isLogin, PesananController.index);
+router.get("/pesanan/create", verifyUser.isLogin, PesananController.create);
+router.post("/pesanan/store", verifyUser.isLogin, (req, res) => {
+    PesananController.store
+});
+router.get("/pesanan/edit/:id", verifyUser.isLogin, PesananController.edit);
+router.post("/pesanan/update/:id", verifyUser.isLogin, (req,res) => {
+    PesananController.update
+});
+router.post("/pesanan/delete/:id", verifyUser.isLogin, (req, res) => {
+   PesananController.destroy 
+}) 
+
+
 
 module.exports = router;
